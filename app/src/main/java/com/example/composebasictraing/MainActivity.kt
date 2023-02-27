9510474e99df35590b1d77edfae9e4d4fce43e36
Abdul wahab <m.wahab@nextgeni.com>
2023-02-27 13:09:46 +0500

    intro toturial complete

--- app/src/main/java/com/example/composebasictraing/MainActivity.kt
+++ app/src/main/java/com/example/composebasictraing/MainActivity.kt
@@ -1,20 +1,25 @@
 package com.example.composebasictraing
 
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.animateContentSize
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.semantics.Role.Companion.Image
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.composebasictraing.ui.theme.ComposeBasicTraingTheme
@@ -29,17 +34,28 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background,
                 ) {
-                    MessageCard()
+
+                    Conversation(getSampleData())
                 }
             }
         }
     }
 }
 
+
 @Composable
-fun MessageCard() {
+fun Conversation(messages: List<Message>) {
+    LazyColumn {
+        items(messages) {
+            MessageCard(message = it)
+        }
+    }
+}
+
+@Composable
+fun MessageCard(message: Message) {
     // Add padding around our message
-    Row(modifier = Modifier.padding(all = 8.dp)) {
+    Row(modifier = Modifier.padding(all = 8.dp).fillMaxSize()) {
         Image(
             painter = painterResource(R.drawable.ic_launcher_background),
             contentDescription = "Contact profile picture",
@@ -54,22 +70,83 @@ fun MessageCard() {
         // Add a horizontal space between the image and the column
         Spacer(modifier = Modifier.width(8.dp))
 
-        Column {
-            Text(text = "wahab", color = MaterialTheme.colors.secondaryVariant,
+        var isExpanded by remember {
+            mutableStateOf(false)
+        }
+
+        val surfaceColor by animateColorAsState(
+            if (isExpanded) MaterialTheme.colors.primary else MaterialTheme.colors.surface,
+        )
+
+        Column(modifier = Modifier.clickable {
+            isExpanded = !isExpanded
+        }) {
+            Text(
+                text = message.auther,
+                color = MaterialTheme.colors.secondaryVariant,
                 style = MaterialTheme.typography.subtitle2
             )
             // Add a vertical space between the author and message texts
             Spacer(modifier = Modifier.height(4.dp))
-            Text(text = "hello", style = MaterialTheme.typography.body2,    modifier = Modifier.padding(all = 4.dp))
+
+            Surface(
+                shape = MaterialTheme.shapes.medium,
+                elevation = 1.dp,
+                // surfaceColor color will be changing gradually from primary to surface
+                color = surfaceColor,
+                // animateContentSize will change the Surface size gradually
+                modifier = Modifier.animateContentSize().padding(1.dp)
+            ) {
+                Text(
+                    text = message.description,
+                    modifier = Modifier.padding(all = 4.dp),
+                    // If the message is expanded, we display all its content
+                    // otherwise we only display the first line
+                    maxLines = if (isExpanded) Int.MAX_VALUE else 1,
+                    style = MaterialTheme.typography.body2
+                )
+            }
 
         }
     }
 }
 
-@Preview(showBackground = true)
+@Preview(showBackground = true, name = "list of messages")
 @Composable
 fun DefaultPreview() {
     ComposeBasicTraingTheme {
-        MessageCard()
+
+        Conversation(getSampleData())
     }
 }
+
+
+private fun getSampleData(): ArrayList<Message> {
+    val list = arrayListOf<Message>().apply {
+        add(
+            Message(
+                "Wahab",
+                "hi compose hi composehi compose hi compose hi compose hi compose hi compose hi compose hi compose"
+            )
+        )
+        add(Message("Wahab", "hi compose"))
+        add(Message("Wahab", "hi compose"))
+        add(Message("Wahab", "hi compose"))
+        add(Message("Wahab", "hi compose"))
+        add(Message("Wahab", "hi compose"))
+    }
+    return list
+}
+
+@Preview(name = "Light Mode")
+@Preview(
+    uiMode = Configuration.UI_MODE_NIGHT_YES, showBackground = true, name = "Dark Mode"
+)
+@Composable
+fun PreviewMessageCard() {
+    ComposeBasicTraingTheme {
+        MessageCard(Message("Wahab", "hi compose"))
+    }
+}
+
+data class Message(val auther: String, val description: String)
